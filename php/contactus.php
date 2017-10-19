649a0edcb18e7f5b040e437b17efb4b29f6e9273
Sahil Tara <supertfriend@gmail.com>
2017-10-19 03:20:25 -0400

    removed 2 unnecessary lines from the test php file [Note: Still just playing around with it so the code might look completely different

--- php/contactus.php
+++ php/contactus.php
@@ -5,9 +5,7 @@ $name = $email = $message;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = test_input($_POST["name"]);
   $email = test_input($_POST["email"]);
-  $website = test_input($_POST["website"]);
   $comment = test_input($_POST["comment"]);
-  $gender = test_input($_POST["gender"]);
 }
 
 function test_input($data) {
