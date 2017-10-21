f97657f97a4c40c19171a70ab00b76b8292272c6
Sahil Tara <supertfriend@gmail.com>
2017-10-21 00:10:28 -0400

    removed dummy links that held the function of allowing page jumping and replaced them with an id tag in their associated divs.

--- php/contactus.php
+++ php/contactus.php
@@ -1,11 +1,11 @@
 <?php
 // define variables and set to empty values
-$name = $email = $message;
+$name = $email = $message = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = test_input($_POST["name"]);
   $email = test_input($_POST["email"]);
-  $comment = test_input($_POST["comment"]);
+  $message = test_input($_POST["comment"]);
 }
 
 function test_input($data) {
@@ -14,5 +14,5 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
-mail("tflowtechnologies@gmail.com")
+mail("tflowtechnologies@gmail.com", "CONTACT US FORM " . $name, $message, "From: " . $name . "<" . $email . ">");
 ?>
