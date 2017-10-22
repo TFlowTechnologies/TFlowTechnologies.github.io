d263367d35a8e98ee3264a5c9a644ef75587db1a
Sahil Tara <supertfriend@gmail.com>
2017-10-21 21:03:33 -0400

    Added smooth scrolling :)! Had a bunch of issues at first but turns out I misnamed something (added an s at the end) then turns out that I forgot to account for the nav bar so I ended up removing the line of code that was messing with me earlier altogether... Anyways There's a nice scrolling feature now try it out!!!

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,6 @@
+//Opacity Nav Bar
 function CheckScroll(){
-	var y = $('.main-header').height() * 2; 
+	var y = $('.main-header').height() * 3; 
 
 	if($(window).scrollTop() > y){
 		$('.main-header').addClass("notattop");
@@ -10,4 +11,21 @@ function CheckScroll(){
 
 $(window).scroll(function() {
     CheckScroll();
+});
+
+//Smooth scrolling
+$(document).ready(function(){
+	//Link click event.
+	$("a").on('click', function(e){
+		if(this.hash != ""){
+			e.preventDefault();
+
+			var hash = this.hash;
+			//animate the scroll
+			$('html, body').animate({
+				//scrolls to location but keeps space for nav bar too!
+				scrollTop: $(hash).offset().top - $('.main-header').height()
+			}, 1000);
+		}
+	});
 });
